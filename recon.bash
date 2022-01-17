eb603b61339efcc2d26e7fa90388ca18f89607af
d3sca <86044770+d3sca@users.noreply.github.com>
2022-01-17 16:19:42 +0000

    Add files via upload

---
+++ recon.bash
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+echo "
+DESCARECON
+";
+
+nuclei --update-templates --silent
+
+read -p "Enter domain names seperated by 'space' : " input
+for i in ${input[@]}
+do
+
+echo "
+.
+.
+.
+Scan started for $i
+" | notify --silent
+
+mkdir $i
+
+assetfinder -subs-only $i >> $i/assetfinder.txt
+subfinder -d $i -o $i/subfinder.txt
+cat $i/assetfinder.txt $i/subfinder.txt > $i/subf.txt
+amass enum --config /home/desca/bb/tools/config.ini --scripts /root/tools/scripts -passive -src -d $i -dir .
+cat amass.txt | awk -F "]" '{print $2}' > $i/amass.txt
+echo "subdomains saved at $i/amass.txt."
+cat $i/amass.txt $i/subf.txt > $i/non-http_list.txt
+cat $i/non-http_list.txt | httpx > $i/subdomains.txt
+echo "subdomains saved at $i/subdomains.txt."
+rm amass.txt
+echo "Scan for default-logins started."
+nuclei -l $i/subdomains.txt -t /home/desca/nuclei-templates/default-logins/ -o $i/default-logins.txt
+echo "Scan for default-logins completed." 
+echo "Scan for exposures started." 
+nuclei -l $i/subdomains.txt -t /home/desca/nuclei-templates/exposures/ -o $i/exposures.txt 
+echo "Scan for exposures completed." 
+echo "Scan for misconfigurations started."
+nuclei -l $i/subdomains.txt -t /home/desca/nuclei-templates/misconfiguration/ -o $i/misconfiguration.txt
+echo "Scan for misconfigurations completed." 
+echo "Scan for takeovers started." 
+nuclei -l $i/subdomains.txt -t /home/desca/nuclei-templates/takeovers/ -o $i/takeovers.txt
+echo "Scan for takeovers completed." 
+echo "Scan for vulnerabilities started."
+nuclei -l $i/subdomains.txt -t /home/desca/nuclei-templates/vulnerabilities/ -o $i/vulnerabilities.txt
+echo "Scan for vulnerabilities completed." 
+echo "Scan for exposed-panels started."
+nuclei -l $i/subdomains.txt -t /home/desca/nuclei-templates/exposed-panels/ -o $i/exposed-panels.txt
+echo "Scan for exposed-panels completed." 
+echo "
+.
+.
+.
+Scan finished for $i
+" | notify --silent
+done
